e84a236d21a178c33b92dcb5e4a2ce5502d6bc4a
GolovinaElia <eliagolovina@ukr.net>
2021-05-18 12:48:33 +0300

    Добавляет файлы

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import style from './ContactForm.module.css';
+import { v4 as uuidv4 } from 'uuid';
 
 class ContactForm extends Component {
   state = {
@@ -14,7 +15,8 @@ class ContactForm extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    this.props.onSubmit(this.state);
+    const { name, number } = this.state;
+    this.props.onSubmit({ id: uuidv4(), name: name, number: number });
     this.reset();
   };
 
@@ -35,6 +37,7 @@ class ContactForm extends Component {
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
               value={this.state.name}
               onChange={this.handleChange}
             />
@@ -48,6 +51,7 @@ class ContactForm extends Component {
               name="number"
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+              required
               value={this.state.number}
               onChange={this.handleChange}
             />
